ffc0a89205ac20138dc909ad07e662a0f1b18d4b
Lucas M. Wilczynski <contact@wilczekpl.com>
2012-12-07 17:11:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TourismFan::Application.config.secret_token = '8ec2e04060450991e9a10a83638d891921f5af03499d5c05d41e299259f1d6f6e9c6b3e33d9e8bd06b1d1487a4426adb1867bde24488abbc5f62655fcf83bc95'
